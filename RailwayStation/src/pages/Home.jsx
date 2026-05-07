b197e67c3e09ab5b36c870326a595b44de7d91e6
Valentyna <mikulik.valentina@gmail.com>
2026-05-07 07:48:06 +0300

    Об'єднання з API для отримання списку потягів

--- RailwayStation/src/pages/Home.jsx
+++ RailwayStation/src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react'
+import { TrainList } from '../components/TrainList'
+
 export const Home = () => {
+  const [trains, setTrains] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/trains')
+      .then(res => res.json())
+      .then(data => setTrains(data))
+  }, [])
+
   return (
-    <div className="container">
+    <div>
       <h2>Розклад потягів</h2>
+      <TrainList trains={trains} />
     </div>
   )
 }
