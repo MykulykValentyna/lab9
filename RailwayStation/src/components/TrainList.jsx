220a4a409b87cd2752c732e2a2f081920c30666f
Valentyna <mikulik.valentina@gmail.com>
2026-05-07 08:27:49 +0300

    Стилізовано список

--- RailwayStation/src/components/TrainList.jsx
+++ RailwayStation/src/components/TrainList.jsx
@@ -1,8 +1,9 @@
 import { TrainCard } from './TrainCard'
+import styles from './TrainList.module.css'
 
 export const TrainList = ({ trains }) => {
   return (
-    <div>
+    <div className={styles.list}>
       {trains.map(train => (
         <TrainCard key={train.id} train={train} />
       ))}
